5911d7b53fe515b6a18ef750454776dae2c3b881
zhn2174 <zhn2174@gmail.com>
2019-03-03 18:44:25 +0800

    a

---
+++ app/src/main/java/com/icebroken/ui/main/adapter/DynamicAdpter.java
@@ -0,0 +1,22 @@
+package com.icebroken.ui.main.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+public class DynamicAdpter extends BaseQuickAdapter {
+    public DynamicAdpter(int layoutResId) {
+        super(layoutResId);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Object item) {
+
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+}
--- app/src/main/java/com/icebroken/ui/main/fragment/SameSchoolFragment.java
+++ app/src/main/java/com/icebroken/ui/main/fragment/SameSchoolFragment.java
@@ -1,9 +1,16 @@
 package com.icebroken.ui.main.fragment;
 
+import android.support.v7.widget.RecyclerView;
+
 import com.icebroken.R;
 import com.icebroken.base.BaseFragment;
 
+import butterknife.Bind;
+
 public class SameSchoolFragment extends BaseFragment {
+    @Bind(R.id.same_school_rv)
+    RecyclerView same_school_rv;
+
     @Override
     protected int getLayoutResource() {
         return R.layout.fragment_same_school;
